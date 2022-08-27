e27e35f639c7f79eac8f04ab3131854d06e7d36f
shadow <757748953@qq.com>
2022-08-27 17:56:38 +0800

    simple job 支持返回值

--- src/main/java/com/shadow/core/javassist/loadtime/SimpleJobJavassistHandler.java
+++ src/main/java/com/shadow/core/javassist/loadtime/SimpleJobJavassistHandler.java
@@ -17,14 +17,16 @@ public class SimpleJobJavassistHandler extends AbstractJavassistHandler {
         return () -> {
             StringBuilder body = new StringBuilder();
             body.append("{");
+            body.append("\n  Object res = null;");
             body.append(setThreadLocal());
             body.append("\n    java.lang.String beanName = com.shadow.supports.helper.ScheduleTaskInfoEnum.getScheduleTaskBeanNameByTaskKey($1);");
             body.append("\n    com.shadow.supports.framework.ICronTriggerTask triggerTask = ");
             body.append(getArgs().get(Constants.IOC_FIELD_NAME));
             body.append(".getBean(beanName, com.shadow.supports.framework.ICronTriggerTask.class);");
             body.append("\n    triggerTask.run();");
+            body.append("\n    res = triggerTask.getResult();");
             body.append(removeThreadLocal());
-            body.append("\n    return \"Successful execute task job : task key = \" + $1 + \" params = \" + $2 + \" body = \" + $3;");
+            body.append("\n    return res;");
             body.append("\n}");
             return body.toString();
         };
